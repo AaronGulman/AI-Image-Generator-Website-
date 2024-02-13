ca68d8b3b3bf68056ceececea8dc03f1f0b01427
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-02-12 21:21:58 -0500

    Finish up the code and fix small issues that prevent from generating images

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 const generateForm = document.querySelector('.generate-form');
+const generateBtn = generateForm.querySelector(".generate-btn");
 const imageGallery = document.querySelector('.image-gallery');
 
-const OPENAI_API_KEY = "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";
+const OPENAI_API_KEY = "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";
 let isImageGenerating = false;
 
 const  updateImageCard = (imgDataArray) => {
 	imgDataArray.forEach((imgObject, index) => {
-		const imgCard = imageGallery.querySelector('.img-card')[index];
+		const imgCard = imageGallery.querySelectorAll('.img-card')[index];
 		const imgElement = imgCard.querySelector('img');
 		const downloadBtn = imgCard.querySelector('.download-btn')
 
-		const aiGeneratedImg = `data:image/jpeg;base64,${imgObject.b64_json}`;
-		imgElement.src = aiGeneratedImg;
+		const aiGeneratedImage = `data:image/jpeg;base64,${imgObject.b64_json}`;
+		imgElement.src = aiGeneratedImage;
 
 
 		imgElement.onload = () => {
 			imgCard.classList.remove("loading");
-			downloadBtn.setAttribute('href,aiGeneratedImg')
-			downloadBtn.setAttribute('download',`${new Date().getTime()}.jpg`)
-
+			downloadBtn.setAttribute('href,aiGeneratedImage')
+			downloadBtn.setAttribute('download',`${new Date().getTime()}.jpg`);
 		}
 	});
 }
@@ -28,25 +28,27 @@ const generateAiImages = async (userPrompt , userImgQuantity) =>{
 		const response = await fetch("https://api.openai.com/v1/images/generations", {
 			method: "POST",
 			headers: {
-				"Content-Type": "application/json",
-				Authorization: `Bearer ${OPENAI_API_KEY}`
+			  "Content-Type": "application/json",
+			  "Authorization": `Bearer ${OPENAI_API_KEY}`,
 			},
 			body: JSON.stringify({
 				prompt: userPrompt,
 				n: userImgQuantity,
 				size: "512x512",
 				response_format: "b64_json"
-			}),
-		});
+			      }),
+			    });			
 		if(!response.ok) throw new Error("Failed to generate images! Please try again.");
 
 
 
-		const {data} = await response.json();
+		const { data } = await response.json();
 		updateImageCard([...data]);
 	}catch(error){
 		alert(error.message);
 	}finally{
+		generateBtn.removeAttribute("disabled");
+		generateBtn.innerText = "Generate";
 		isImageGenerating = false;
 	}
 }
@@ -59,13 +61,17 @@ const handleFormSubmission = (e) => {
 
 
 	const userPrompt = e.srcElement[0].value;
-	const userImgQuantity = e.srcElement[1].value;
+	const userImgQuantity = parseInt(e.srcElement[1].value);
+
+	generateBtn.setAttribute("disabled", true);
+	generateBtn.innerText = "Generating";
+	isImageGenerating = true;
 
 	const imgCardMarkup = Array.from({length: userImgQuantity}, () =>
 		`<div class="img-card loading">
 		<img src="./assets/loader.svg" alt="loading picture">
 		<a href="#" class="download-btn">
-			<img class="download" src="./assets/download.svg" alt="">
+			<img src="./assets/download.svg" alt="download icon">
 		</a>
 	</div>`
 	).join('');
